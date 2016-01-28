4f62597e670867dff417add4a23502d8b3f4f108
Kashav Madan <kshvmdn@gmail.com>
2016-01-28 13:17:11 -0500

    Add functionality to listen for various phrases

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from twitterbot import TwitterBot
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 parser = argparse.ArgumentParser(description='Respond to Twitter mentions.')
-parser.add_argument('-l', '--listen', metavar='', default='happy birthday',
-                    help='mention text to listen for')
+parser.add_argument('-l', '--listen', metavar='', nargs='+', default='happy birthday',
+                    help='phrase[s] to reply to (separate by space)')
 parser.add_argument('-r', '--response', metavar='', default='HANDLE thanks!',
                     help='response text (use HANDLE for user handle)')
 args = parser.parse_args()
@@ -22,12 +22,13 @@ def respond():
 
 
 def main():
-    print('Starting bot...')
-
-    # run once every minute
-    scheduler = BlockingScheduler()
-    scheduler.add_job(respond, 'interval', minutes=1)
-    scheduler.start()
+    # print('Starting bot...')
+    #
+    # # run once every minute
+    # scheduler = BlockingScheduler()
+    # scheduler.add_job(respond, 'interval', minutes=1)
+    # scheduler.start()
+    respond()
 
 if __name__ == '__main__':
     main()
--- twitterbot.py
+++ twitterbot.py
@@ -3,7 +3,7 @@ import time
 
 
 class TwitterBot:
-    def __init__(self, twitter_auth, listen_msg, response_msg):
+    def __init__(self, twitter_auth, listen_list, response_msg):
 
         auth = tweepy.OAuthHandler(twitter_auth['consumer_key'],
                                    twitter_auth['consumer_secret'])
@@ -11,7 +11,7 @@ class TwitterBot:
                               twitter_auth['access_token_secret'])
         self.api = tweepy.API(auth)
         self.responded_tweets = []
-        self.listen, self.response = listen_msg, response_msg
+        self.listen, self.response = listen_list, response_msg
 
     def tweet(self, message, mention_id=None):
         self.api.update_status(status=message, in_reply_to_status_id=mention_id)
@@ -19,7 +19,7 @@ class TwitterBot:
     def respond_to_mention(self):
         print('  Searching mentions...')
         for mention in self.api.mentions_timeline():
-            if self.listen.lower() in mention.text.lower() \
+            if any(t in mention.text.lower() for t in self.listen) \
                     and mention.id not in self.responded_tweets:
                 print('   Found tweet - {}'.format(mention.text))
                 try:
